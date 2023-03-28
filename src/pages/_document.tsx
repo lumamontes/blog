4c340970a24bbf31b067d26e197adf80a4f69ba0
luma <lumagoesmontes@gmail.com>
2023-03-28 00:12:31 -0300

    listagem de posts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,39 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const originalRenderPage = ctx.renderPage
+
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      })
+
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
+import { createClient } from '../../prismicio';
+
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { RichText } from 'prismic-dom';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 interface Post {
   uid?: string;
@@ -24,13 +31,77 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({postWithPagination}) {
+  console.log(postWithPagination)
+  return (
+    <section className={styles.container}>
+      <div>
+          <img src={'/Logo.svg'} alt='logo'/>
+      </div>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+      <div className={styles.posts}>
+          {
+            postWithPagination.results.map(post => (
+                <Link href={`/posts/preview/${post.uid}`}>
+                    <div key={post.uid}>
+                        <h1>{post.data.title}</h1>
+                        <p>{post.data.subtitle}</p>
+                        <div className={styles.info}>
+                          <time>
+                              <FiCalendar className={styles.icon} />
+                              {post.first_publication_date}
+                          </time>
+                          <p>
+                            <FiUser className={styles.icon}/>
+                            {post.data.author}
+                          </p>
+                        </div>
+                    </div>
+                </Link>
+              ))
+          }
+      </div>
+      {
+        postWithPagination.next_page && (
+          <div className={styles.continueReading}>
+          <a> 
+            Carregar mais posts
+            </a>
+        </div>
+        )
+      }
+    
 
-//   // TODO
-// };
+    </section>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  
+  const client = createClient();
+  const postsResponse = await client.getByType('post');
+  const postWithPagination: PostPagination = {
+    next_page: postsResponse.next_page,
+    results: postsResponse.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date:new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author
+        }
+      }
+    })
+  }
+  
+  return {
+    props: {
+      postWithPagination, 
+     }
+  }
+};
